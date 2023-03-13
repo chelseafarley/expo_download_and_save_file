e4357dea0b9f0fc00b17a61d55b75bb13a42de7d
Chelsea <chelsea@tripwiretech.com>
2023-03-13 19:24:03 +1300

    Download files and save to file app

--- App.js
+++ App.js
@@ -1,10 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, Platform } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import { shareAsync } from 'expo-sharing';
 
 export default function App() {
+  const downloadFromUrl = async () => {
+    const filename = "small.mp4";
+    const result = await FileSystem.downloadAsync(
+      'http://techslides.com/demos/sample-videos/small.mp4',
+      FileSystem.documentDirectory + filename
+    );
+    console.log(result);
+
+    save(result.uri, filename, result.headers["Content-Type"]);
+  };
+
+  const downloadFromAPI = async () => {
+    const filename = "MissCoding.pdf";
+    const localhost = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
+    const result = await FileSystem.downloadAsync(
+      `http://${localhost}:5000/generate-pdf?name=MissCoding&email=hello@tripwiretech.com`,
+      FileSystem.documentDirectory + filename,
+      {
+        headers: {
+          "MyHeader": "MyValue"
+        }
+      }
+    );
+    console.log(result);
+    save(result.uri, filename, result.headers["Content-Type"]);
+  };
+
+  const save = async (uri, filename, mimetype) => {
+    if (Platform.OS === "android") {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (permissions.granted) {
+        const base64 = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
+        await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, filename, mimetype)
+          .then(async (uri) => {
+            await FileSystem.writeAsStringAsync(uri, base64, { encoding: FileSystem.EncodingType.Base64 });
+          })
+          .catch(e => console.log(e));
+      } else {
+        shareAsync(uri);
+      }
+    } else {
+      shareAsync(uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Download From URL" onPress={downloadFromUrl} />
+      <Button title="Download From API" onPress={downloadFromAPI} />
       <StatusBar style="auto" />
     </View>
   );
